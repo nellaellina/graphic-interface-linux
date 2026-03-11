dc12c8b70cd44cfbe8f4557d4d910405478d0a41
nellaellina <casamaddalena18@gmail.com>
2026-03-11 16:23:08 +0100

    Create basic dock implementation file

---
+++ src/dock/dock.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <graphics.h> // Hypothetical graphics library
+
+void renderDock() {
+    // Basic dock rendering logic
+    printf("Rendering dock...\n");
+    // Implementation would go here
+}
+
+void animateDock() {
+    // Basic animation logic
+    printf("Animating dock...\n");
+    // Animation code would go here
+}
+
+int main() {
+    renderDock();
+    animateDock();
+    
+    return 0;
+}
